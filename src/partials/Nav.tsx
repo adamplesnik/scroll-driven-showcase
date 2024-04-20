3103f784aeffdb496ee99e0bad643e96ff092acd
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-20 20:35:13 +0200

    Style the nav

--- src/partials/Nav.tsx
+++ src/partials/Nav.tsx
@@ -4,7 +4,13 @@ import DarkModeSwitch from './DarkModeSwitch'
 
 const Nav = ({ className, ...rest }: HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className={'flex gap-4 ' + addWithSpace(className)} {...rest}>
+    <div
+      className={
+        'flex items-center gap-4 rounded-full bg-slate-700/10 p-4 shadow-2xl backdrop-blur dark:bg-slate-400/10' +
+        addWithSpace(className)
+      }
+      {...rest}
+    >
       nav
       <DarkModeSwitch />
     </div>
