c1252cfcc934cdf3434ade73ad652a91a397692e
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-22 15:21:45 +0200

    Tune layouts

--- src/layouts/BaseWithNav.tsx
+++ src/layouts/BaseWithNav.tsx
@@ -3,10 +3,10 @@ import Nav from '../partials/Nav'
 
 const BaseWithNav = ({ children }: PropsWithChildren<BaseWithNavProps>) => {
   return (
-    <div>
+    <>
       {children}
       <Nav className="fixed bottom-8 left-6 z-50" />
-    </div>
+    </>
   )
 }
 
