779a061080d68dd006fc1475897687391b93679b
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-22 22:05:00 +0200

    Tune scroll-down component

--- src/components/ScrollDown.tsx
+++ src/components/ScrollDown.tsx
@@ -2,10 +2,8 @@ import { ArrowDown } from 'lucide-react'
 
 const ScrollDown = () => {
   return (
-    <div className="absolute z-[500] flex w-full justify-center p-4">
-      <div className="flex w-fit gap-2 rounded-full bg-white/0 p-4 text-sm shadow-2xl backdrop-blur">
-        <ArrowDown className="animate-bounce" /> Scroll Down
-      </div>
+    <div className="absolute z-50 flex w-full justify-center gap-2 p-8 text-sm font-medium">
+      <ArrowDown className="size-5 animate-bounce" /> Scroll Down
     </div>
   )
 }
