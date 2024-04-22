0a6381a1aa7877a833e732042e13f08e8025c07f
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-22 22:02:40 +0200

    Move keyframes to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,39 @@ export default {
         'stroke-opacity':
           'to-opacity-full 5s ease-in-out forwards, to-translate-x-0 5s ease-in-out forwards',
       },
+      keyframes: {
+        gradient: {
+          from: {
+            backgroundSize: '400% 100%',
+          },
+          '50%': {
+            backgroundSize: '200% 100%',
+          },
+          '70%': {
+            backgroundSize: '100% 100%',
+            opacity: 1,
+          },
+          '98%': {
+            opacity: 0,
+          },
+          to: {
+            backgroundSize: '44% 100%',
+            opacity: 0,
+          },
+        },
+        'to-max-width-unset': {
+          to: { maxWidth: 'unset' },
+        },
+        'to-opacity-full': {
+          to: { opacity: 1 },
+        },
+        'to-stroke-dashoffset-0': {
+          to: { strokeDashoffset: 0 },
+        },
+        'to-translate-x-0': {
+          to: { transform: 'translateX(0)' },
+        },
+      },
       transitionTimingFunction: {
         line: 'cubic-bezier(0.65, 0.05, 0.17, 0.99)',
       },
