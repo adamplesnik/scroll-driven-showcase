3b99dbce0c827fe6cc2c2aa7051b4b27c1ebb6ec
Adam Plesnik <adam.plesnik@expersoft.com>
2024-04-23 22:13:42 +0200

    Tune amenities

--- src/pages/forest/Amenities.tsx
+++ src/pages/forest/Amenities.tsx
@@ -8,16 +8,16 @@ import {
   Stars,
   Utensils,
 } from 'lucide-react'
-import { useState } from 'react'
-import viewImage from './assets/view.jpg'
+import CodeBlock from '../../components/CodeBlock'
+import TimelineMarker from '../../components/TimelineMarker'
+
+const codeExample = ``
 
 type listType = {
   label: string
   desc: string
   icon: LucideIcon
-  clickable?: boolean | false
-  detail?: string | undefined
-  detailImg?: string | undefined
+  code?: string
 }
 
 const list: listType[] = [
@@ -25,9 +25,6 @@ const list: listType[] = [
     label: 'Living Area',
     desc: 'Cozy space with a gorgeous view',
     icon: Stars,
-    clickable: true,
-    detail: "Money can't buy happiness, but they can definitely but this view.",
-    detailImg: viewImage,
   },
   {
     label: 'Two Bedrooms',
@@ -42,43 +39,62 @@ const list: listType[] = [
     desc: 'Explore the surrounding forest',
     icon: MapPinned,
   },
-  { label: 'Parking', desc: 'Space for your vehicle', icon: Car },
+  {
+    label: 'Parking',
+    desc: 'Space for your vehicle',
+    icon: Car,
+    code: '<li class="animate-to-translate-x-0 timeline-view range-on-entry/64px-144px translate-x-[30vw]">',
+  },
 ]
 
-const Amenities = () => {
-  const [clicked, setClicked] = useState(false)
+const jsExample = `matchUtilities(
+  {
+    'dash-array': (value, { modifier }) => ({
+      strokeDasharray: modifier,
+    }),
+  },
+  { values: { DEFAULT: '' }, modifiers: 'any' }
+)
+addVariant('path', '& > path')`
 
+const Amenities = () => {
   return (
-    <ul className="mb-24 mt-16 flex flex-col gap-4">
-      {list.map((item, i) => (
-        <li
-          className={
-            'flex translate-x-[30vw] animate-stroke-opacity gap-4 rounded-lg bg-slate-200/30 px-4 py-2 opacity-0 timeline-view range-on-entry/100px-160px dark:bg-zinc-800/60' +
-            (item.clickable ? ' cursor-pointer transition-transform hover:scale-110' : '')
-          }
-          onClick={() => item.clickable && setClicked(!clicked)}
-          key={i}
-        >
-          <item.icon className="mt-2 flex-shrink-0 text-slate-500 path:animate-to-stroke-dashoffset-0 path:timeline-view path:range-on-entry/100%-400px path:dash-offset/-50 path:dash-array/100" />{' '}
-          <div className="flex w-full -translate-x-10 animate-to-translate-x-0 flex-col gap-1 timeline-view range-on-entry/20%-40%">
-            <span className="text-zinc-950 dark:text-zinc-100">{item.label}</span>
-            <span className="text-sm">{item.desc}</span>
-            <div
-              className={
-                'h-0 gap-4 overflow-hidden transition-[height] duration-500' +
-                (clicked ? ' h-fit' : '') +
-                (item.detail ? ' flex flex-col' : ' hidden')
-              }
+    <div className="mb-24 mt-24">
+      <div className="relative">
+        <ul className="mb-28 flex flex-col gap-4">
+          {list.map((item, i) => (
+            <li
+              className="flex translate-x-[30vw] animate-stroke-opacity gap-4 rounded-lg bg-slate-200/30 px-4 py-2 opacity-0 timeline-view range-on-entry/64px-144px dark:bg-zinc-800/60"
+              key={i}
             >
-              <div className="flex h-80 w-full items-end overflow-hidden rounded-lg">
-                <img src={item.detailImg} className="" />
+              <item.icon className="mt-2 flex-shrink-0 text-slate-500 path:animate-to-stroke-dashoffset-0 path:timeline-view path:range-on-entry/100%-400px path:dash-offset/-50 path:dash-array/100" />{' '}
+              <div className="flex min-w-0 flex-1 flex-col gap-2">
+                <span className="text-zinc-950 dark:text-zinc-100">{item.label}</span>
+                <span className="text-sm">{item.desc}</span>
+                {item.code && <CodeBlock language="html">{item.code}</CodeBlock>}
               </div>
-              {item.detail}
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+            </li>
+          ))}
+        </ul>
+        <TimelineMarker
+          className="absolute bottom-[135px] left-0 right-0 z-50"
+          textStart="0px"
+          textEnd="`on-entry`"
+        />
+        <TimelineMarker
+          className="absolute bottom-[71px] left-0 right-0 z-50"
+          textStart="100px"
+          textEnd="0%"
+        />
+        <TimelineMarker
+          className="absolute -bottom-[9px] left-0 right-0 z-50"
+          textStart="160px"
+          textEnd="100%"
+        />
+      </div>
+      <CodeBlock language="javascript">{jsExample}</CodeBlock>
+      <CodeBlock language="html">{codeExample}</CodeBlock>
+    </div>
   )
 }
 
