ab3823533ebd98bc1af702a7e103f8c8d3078cbc
Adam Plesnik <adam.plesnik@expersoft.com>
2024-05-02 23:04:46 +0200

    Tune nav

--- src/components/ScrollDown.tsx
+++ src/components/ScrollDown.tsx
@@ -2,7 +2,7 @@ import { ArrowDown } from 'lucide-react'
 
 const ScrollDown = () => {
   return (
-    <div className="absolute z-40 flex w-full justify-center gap-2 p-8 pt-24 text-sm font-medium md:pt-10">
+    <div className="absolute z-40 flex w-full justify-center gap-2 p-8 pt-24 text-sm font-medium">
       <ArrowDown className="size-5 animate-bounce" /> Scroll Down
     </div>
   )
--- src/partials/Nav.tsx
+++ src/partials/Nav.tsx
@@ -4,27 +4,33 @@ import NavItem from '../components/NavItem'
 import { addWithSpace } from '../utils/addWithSpace'
 import DarkModeSwitch from './DarkModeSwitch'
 import TechInfoSwitch from './TechInfoSwitch'
+import CenterLayout from '../layouts/CenterLayout'
 
 const Nav = ({ className, ...rest }: HTMLAttributes<HTMLDivElement>) => {
   return (
     <div
       className={
-        'flex items-center gap-2 bg-slate-100/40 px-4 shadow-2xl shadow-black/5 backdrop-blur md:rounded-full dark:bg-slate-400/20' +
+        'border-b border-b-slate-500/50 bg-slate-100/40 shadow-2xl shadow-black/5 backdrop-blur dark:border-b-slate-300/50 dark:bg-zinc-900/50' +
         addWithSpace(className)
       }
       {...rest}
     >
-      <NavItem Icon={AlignVerticalJustifyEnd} to="/" />
-      <NavItem Icon={Trees} to="/forest" />
-      <NavItem Icon={List} to="/list" />
-      <div className="min-w-8 flex-1" />
-      <DarkModeSwitch />
-      <TechInfoSwitch />
-      <NavItem
-        Icon={Github}
-        to="https://github.com/adamplesnik/scroll-driven-playground"
-        target="_blank"
-      />
+      <CenterLayout className="relative flex items-center gap-2">
+        <h1 className="mr-4 text-sm font-semibold text-zinc-800 dark:text-zinc-200">
+          Scroll-driven Playground
+        </h1>
+        <NavItem Icon={AlignVerticalJustifyEnd} to="/" />
+        <NavItem Icon={Trees} to="/forest" />
+        <NavItem Icon={List} to="/list" />
+        <div className="min-w-8 flex-1" />
+        <DarkModeSwitch />
+        <TechInfoSwitch />
+        <NavItem
+          Icon={Github}
+          to="https://github.com/adamplesnik/scroll-driven-playground"
+          target="_blank"
+        />
+      </CenterLayout>
     </div>
   )
 }
